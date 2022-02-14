30c0f2ec78d94acdc2aa9f8ab8671c26bd3a2b83
Axure <baldarzana7@gmail.com>
2022-02-14 17:52:34 +0000

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# math
+# Features
+- Addition, Subtraction, Multiplication and Divition
+
+## Examples
+```js
+const { sum } = require('math-operations-axure)
+
+console.log(sum(1, 2)) // returns 3
+```
+```js
+const { subtract } = require('math-operations-axure)
+
+console.log(subtract(2, 1)) // returns 1
+```
+```js
+const { multiply } = require('math-operations-axure)
+
+console.log(multiply(2, 1)) // returns 2
+```
+```js
+const { divide } = require('math-operations-axure)
+
+console.log(divide(3, 5)) // returns 0.6
+```
+```js
+const { round } = require('math-operations-axure)
+
+console.log(round(3.4)) // returns 3
+```
+```js
+const { round } = require('math-operations-axure)
+
+console.log(round(3.4, 2)) // returns 7
+```
+```js
+const { round } = require('math-operations-axure)
+
+console.log(round(3.4, 2, 2)) // returns 4
+```
+
+## To Do
+- Add Support to Multiply, Divide, Add and Subtract More than Two Numbers
+
+## Notes
+- Use Commas and Not the Symbol ( + - * / )
+- Previous Versions Will Not Work (1.0.0 - 1.0.4)
---
+++ index.js
@@ -0,0 +1,63 @@
+/**
+ * @param {*} a - The First Number
+ * @param {*} b - The Second Number
+ */
+const sum = (a, b) => {
+    if(!a || b || a && b){
+        throw new Error("Please Specifiy two Numbers to Sum.");
+    }
+    return a + b
+}
+
+/**
+ * @param {*} a - The First Number
+ * @param {*} b - The Second Number
+ */
+const subtract = (a, b) => {
+    if(!a || b || a && b){
+        throw new Error("Please Specifiy two Numbers to Subtract.");
+    }
+    return a - b
+}
+
+/**
+ * @param {*} a - The First Number
+ * @param {*} b - The Second Number
+ */
+const multiply = (a, b) => {
+    if(!a || b || a && b){
+        throw new Error("Please Specifiy two Numbers to Multiply.");
+    }
+    return a * b
+}
+
+/**
+ * @param {*} a - The First Number
+ * @param {*} b - The Second Number
+ */
+const divide = (a, b) => {
+    if(!a || b || a && b){
+        throw new Error("Please Specifiy two Numbers to Divide.");
+    }
+    return a/b
+}
+
+/**
+ * @param {*} a - Rounds a Number to the Nearest Integer
+ * @param {*} b - Multiplies A by B and Rounds it to The Nearest Integer
+ * @param {*} c - Multiplies A by B, Divides the Answer by C and Rounds it to The Nearest Integer
+ */
+const round = (a, b, c) => {
+    if(!a){
+        throw new Error("Please Specifiy a Number to Round");
+    }
+    if(!b || c){
+        return Math.round(a)
+    } else if(!c && a && b) {
+        return Math.round(a * b)
+    } else if(a && b && c){
+        return Math.round(a * b) / c
+    }
+}
+
+module.exports = { sum, subtract, multiply, divide, round }
